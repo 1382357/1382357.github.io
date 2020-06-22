a57c6122cf4f1d6a3ad226c40001b8e7284ce9f3
1382357 <ruben.reyesa@e-campus.uab.cat>
2020-06-22 15:47:35 +0200

    First Version

---
+++ precache-manifest.dec2c1a94598336325f880be5bd6e3a3.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2530172c4e6018b35607",
+    "url": "/css/app.1d107219.css"
+  },
+  {
+    "revision": "d3560b174bab99e7c2c2",
+    "url": "/css/chunk-vendors.c663b0b6.css"
+  },
+  {
+    "revision": "edd0880999e0c29514f97cdd9d679def",
+    "url": "/img/nvs.edd08809.png"
+  },
+  {
+    "revision": "d9730d44b83739c057c72ce09b5a4130",
+    "url": "/img/team1.d9730d44.png"
+  },
+  {
+    "revision": "0019350d8103a5f0d9187b848ef5bccf",
+    "url": "/img/team10.0019350d.png"
+  },
+  {
+    "revision": "b1d9e073de6422b65dae2143dc64b584",
+    "url": "/img/team11.b1d9e073.png"
+  },
+  {
+    "revision": "b34edf8971ea461002dfdd4b5f3a8b03",
+    "url": "/img/team12.b34edf89.png"
+  },
+  {
+    "revision": "e18b18c71f78f1a8597c362041526514",
+    "url": "/img/team13.e18b18c7.png"
+  },
+  {
+    "revision": "b991f25c2dda5cc058da0360f0a88863",
+    "url": "/img/team14.b991f25c.png"
+  },
+  {
+    "revision": "8e57e30ba56f55aeb69bfbfaa3516338",
+    "url": "/img/team15.8e57e30b.png"
+  },
+  {
+    "revision": "f35f17b233d6c739cd0f8c3d7892427f",
+    "url": "/img/team16.f35f17b2.png"
+  },
+  {
+    "revision": "ad1d0e338f2fbfcd94be9c18d242737c",
+    "url": "/img/team2.ad1d0e33.png"
+  },
+  {
+    "revision": "7e4ab2aa98c4316e07d3ea1e4258f383",
+    "url": "/img/team3.7e4ab2aa.png"
+  },
+  {
+    "revision": "4f85037532d5104ca777959668d84be8",
+    "url": "/img/team4.4f850375.png"
+  },
+  {
+    "revision": "ac744cb99831d9ffa2ff32a3a85cc275",
+    "url": "/img/team5.ac744cb9.png"
+  },
+  {
+    "revision": "6c9ae295d7623459d1003662277c2f26",
+    "url": "/img/team6.6c9ae295.png"
+  },
+  {
+    "revision": "4f13729a710e0937dbb1226830a03396",
+    "url": "/img/team7.4f13729a.png"
+  },
+  {
+    "revision": "4068bc77a30616e276e74965734f9562",
+    "url": "/img/team8.4068bc77.png"
+  },
+  {
+    "revision": "e11b2daf0ffd8d0270fda28fd7f4effb",
+    "url": "/img/team9.e11b2daf.png"
+  },
+  {
+    "revision": "e341be974ac6461a52255effd296297c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "900f803367370ddaeb8c",
+    "url": "/js/about.b7a980e1.js"
+  },
+  {
+    "revision": "2530172c4e6018b35607",
+    "url": "/js/app.55e2e84d.js"
+  },
+  {
+    "revision": "d3560b174bab99e7c2c2",
+    "url": "/js/chunk-vendors.2b2b85c4.js"
+  },
+  {
+    "revision": "3af0b9aa2e99077fa7a3b99094415dec",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
